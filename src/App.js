85a1515816153ef9b9cb4a4be95140b53be5059b
deveshlashkari <deveshlashkari@gmail.com>
2020-08-06 19:43:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+import "react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min.css";
+
+import BootstrapTable from "react-bootstrap-table-next";
+import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
+
+const { SearchBar } = Search;
+
+const columns = [
+  {
+    dataField: "id",
+    text: "Product ID",
+  },
+  {
+    dataField: "name",
+    text: "Product Name",
+  },
+  {
+    dataField: "price",
+    text: "Product Price",
+  },
+];
+
+const products = [
+  {
+    id: "1",
+    name: "Product 1",
+    price: "100",
+  },
+  {
+    id: "2",
+    name: "Product 2",
+    price: "100",
+  },
+  {
+    id: "3",
+    name: "Product 3",
+    price: "100",
+  },
+];
+const selectAll = (isSelected) => {
+  console.log(isSelected);
+};
+
+const selectRow = {
+  mode: "checkbox",
+  clickToSelect: false,
+
+  onSelectAll: selectAll,
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToolkitProvider keyField="id" data={products} columns={columns} search>
+        {(props) => (
+          <div>
+            <h3>Table</h3>
+            <SearchBar {...props.searchProps} />
+            <hr />
+            <BootstrapTable
+              keyField="id"
+              data={products}
+              selectRow={selectRow}
+              condensed
+              striped
+              columns={columns}
+            />
+          </div>
+        )}
+      </ToolkitProvider>
+    </>
   );
 }
 
